7e5050e11f1bc975d5ecb28d011d17558b484e25
Kumar72 <vichandan@gmail.com>
2017-04-03 17:17:39 -0600

    working on Admin kitchen

--- MVCCaterMeTonight/src/controllers/KitchenController.java
+++ MVCCaterMeTonight/src/controllers/KitchenController.java
@@ -1,14 +1,15 @@
 package controllers;
 
-import javax.enterprise.inject.Model;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import data.KitchenDAO;
+import data.MenuDAO;
+import entities.Kitchen;
 
 @Controller
 public class KitchenController {
@@ -16,12 +17,65 @@ public class KitchenController {
 	@Autowired
 	private KitchenDAO kitchenDao;
 	
-	@RequestMapping(path = "GetKitchen.do",
+	@Autowired
+	private MenuDAO menuDao;
+	
+	@RequestMapping(path = "KitchenController.do",
 		method = RequestMethod.GET)
-	 public ModelAndView getAllKitchen() {
+	 public ModelAndView getAllKitchen(@RequestParam("action")String action,
+			 @RequestParam("kitchenId")int id) {
 		ModelAndView mv = new ModelAndView();
-		mv.setViewName("kitchen.jsp");
-		mv.addObject("kitchens", kitchenDao.listOfKitchen());
+		if (action.equalsIgnoreCase("all")){
+//			System.out.println("################TEST################");
+			mv.setViewName("menu");
+			mv.addObject("selectedMenu", menuDao.(id));
+		}
+		else if (action.equalsIgnoreCase("appetizer")){
+			mv.setViewName("menu");
+			mv.addObject("selectedMenu", menuDao.getAllMenuItemsFromAppetizers(id));
+		}
+		else if(action.equalsIgnoreCase("entree")) {
+			mv.setViewName("menu");
+			mv.addObject("selectedMenu", menuDao.getAllMenuItemsFromEntrees(id));
+		}
+		else if(action.equalsIgnoreCase("dessert")) {
+			mv.setViewName("menu");
+			mv.addObject("selectedMenu", menuDao.getAllMenuItemsFromDesserts(id));
+		}
+		else if(action.equalsIgnoreCase("drink")) {
+			mv.setViewName("menu");
+			mv.addObject("selectedMenu", menuDao.getAllMenuItemsFromDrinks(id));
+		}
+		else if(action.equalsIgnoreCase("edit")) {
+			
+		}
 	 return mv;
 	 }
+	
+	@RequestMapping(path = "CreateKitchen.do",
+			method = RequestMethod.POST)
+		 public ModelAndView createNewKitchen(Kitchen kitchen) {
+			ModelAndView mv = new ModelAndView();
+			mv.setViewName("kitchen");
+			mv.addObject("kitchens", kitchenDao.createKitchen(kitchen));
+		 return mv;
+		 }
+	
+	@RequestMapping(path = "UpdateKitchen.do",
+			method = RequestMethod.POST)
+		 public ModelAndView updateKitchen(int id, Kitchen kitchen) {
+			ModelAndView mv = new ModelAndView();
+			mv.setViewName("kitchen.jsp");
+			mv.addObject("kitchens", kitchenDao.updateKitchen(id, kitchen));
+		 return mv;
+		 }
+	
+	@RequestMapping(path = "RemoveKitchen.do",
+			method = RequestMethod.POST)
+		 public ModelAndView removeKitchen(@RequestParam("delete")int id) {
+			ModelAndView mv = new ModelAndView();
+			mv.setViewName("kitchen.jsp");
+			mv.addObject("kitchens", kitchenDao.removeKitchenAndCuisine(id));
+		 return mv;
+		 }
 }
